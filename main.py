dec084142f7693cc61821d726214ff5a541fdbc1
abbymachines <abby.machines@gmail.com>
2024-09-11 15:30:24 -0700

    setup more effective use of multithreading

--- main.py
+++ main.py
@@ -4,30 +4,12 @@ import threading
 from tama import Tama
 from inputimeout import inputimeout, TimeoutOccurred
 
-def print_cube(num):
-  print("Cube: {}" .format(num * num * num))
-
-def print_square(num):
-  print("Square: {}" .format(num * num))
-
-if __name__=="__main__":
-  t1 = threading.Thread(target=print_square, args=(10,))
-  t2 = threading.Thread(target=print_cube, args=(10,))
-
-# t1 = threading.Thread(target, args)
-# t2 = threading.Thread(target, args)
-
-  t1.start()
-  t2.start()
-
-  t1.join()
-  t2.join()
-
-print("Done!")
-
 pet = Tama(health=0)
 pet.set_body()
 pet.set_name()
+
+def example_for_multithreading(num):
+  print(f"look at me! i love to process multiple threads simultaneously. yep {num}")
 
 def wait_for_input():
   try:
@@ -37,9 +19,25 @@ def wait_for_input():
   except TimeoutOccurred:
       pet.go_hungry()
 
-while pet.is_alive:
-  wait_for_input()
+def pet_lifecycle():
+  while pet.is_alive:
+    wait_for_input()
+    
+    if pet.health < 0:
+      print(f"{pet.name} died :(")
+      pet.is_alive = False
 
-  if pet.health < 0:
-    print(f"{pet.name} died :(")
-    pet.is_alive = False
+if __name__=="__main__":
+  t1 = threading.Thread(target=pet_lifecycle)
+  t2 = threading.Thread(target=example_for_multithreading, args=([10]))
+  
+  t1.start()
+  # t1.join()
+  t2.start()
+  
+  t1.join()
+  t2.join()
+  
+  print("Done!")
+
+# pet_lifecycle()
